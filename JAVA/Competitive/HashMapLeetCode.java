2a991fd1ac16df829ef2fbd1a93899197ef4fa62
Sudhanshu Dhanik <sonudhanik2812@gmail.com>
2024-07-20 12:51:43 +0530

    v

--- JAVA/Competitive/HashMapLeetCode.java
+++ JAVA/Competitive/HashMapLeetCode.java
@@ -3,5 +3,6 @@ public class HashMapLeetCode {
     
    //  https://leetcode.com/problems/count-number-of-nice-subarrays/?envType=daily-question&envId=2024-06-22
     
-    
+   //  HASHMAP AND TREE
+   // https://leetcode.com/problems/create-binary-tree-from-descriptions/submissions/1321465350/?envType=daily-question&envId=2024-07-15
 }
---
+++ JAVA/Competitive/maths/GFG.java
@@ -0,0 +1,6 @@
+package maths;
+
+public class GFG {
+ 
+    //https://www.geeksforgeeks.org/problems/smallest-number5829/1
+}
